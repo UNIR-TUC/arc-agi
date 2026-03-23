0b2611df0f6eac929009dc5625f0bd731d78989a
Asier <blycket12@hotmail.es>
2026-03-23 21:28:26 +0100

    #0: doc: Creating the TFM document

---
+++ Doc/TFM-UNIR/TFM/Plantilla/compilar.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Compilando ..."
+pdflatex plantilla.tex
+echo "✔ Compilación finalizada"
---
+++ Doc/TFM-UNIR/TFM/compilar.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Compilando ..."
+pdflatex TFM_IA_72858458R.tex
+echo "✔ Compilación finalizada"
