19acd2cf5df48aa01fa748635c9c4b1157ffe9d7
Asier Marín Fernández <A.marin@lantek.com>
2026-05-18 17:01:28 +0200

    Refactor TFM document: Update content, structure, and images
    - Revised table of contents for accurate section numbering and titles.
    - Corrected typographical errors and improved formatting in the PDF and LaTeX files.
    - Added new subsections to enhance the clarity of the document structure.
    - Included new images to support the content and added a markdown file for image references.
    - Updated bibliography with a new reference for AMD RDNA 4 GPU.
    - Modified the compilation script to include a cleanup step for temporary files.

--- Doc/TFM-UNIR/TFM/compilar.ps1
+++ Doc/TFM-UNIR/TFM/compilar.ps1
@@ -1,3 +1,6 @@
+Write-Host "Eliminando archivos temporales de compilación..."
+# Remove-Item -Path *.aux, *.log, *.toc, *.out -ErrorAction SilentlyContinue
+
 Write-Host "Compilando el documento LaTeX..."
 pdflatex TFM_IA_72858458R.tex
 Write-Host "Compilación completada."
